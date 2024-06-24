737742f9b724a7d11c521be8036df82dd13fc29f
mrfukui <mr.fukui@yahoo.com>
2024-06-23 21:36:08 -0700

    Added authentication

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "recipe_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [BASE_DIR  / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -114,6 +114,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+#AUTH
+LOGIN_URL='/login/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
